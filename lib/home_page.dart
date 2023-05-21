bbf535311ca0e242b26d6a7a41f39ade334ed7ee
sova <mahatoshobha143@gmail.com>
2023-05-21 17:56:35 +0545

    bird jumps functions added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,45 +11,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double birdYaxis =0;
+  static double birdYaxis = 0;
 
-  //equation 
+  //equation
   //y=-g/2*time^2+velocity*time
   //i.e height=-4.9*time+ 3*time   here g=-9.8, velocity=3
-  double height=0;
-  double time=0;
-  double initialHeight =0;
-  
+  double height = 0;
+  double time = 0;
+  double initialHeight = birdYaxis;
 
+  bool gameHasStarted = false;
 
-  void Jump(){
-    initialHeight=birdYaxis;
-    Timer.periodic(Duration(milliseconds: 50), (timer) {
+  void Jump() {
+    setState(() {
+      time = 0;
       
-       time+=0.4;
-      height = -4.9*time*time+2.8*time;
+      print("jumping------------------------------------------");
+
+      initialHeight = birdYaxis;
+      
+
+    });
+  }
+
+  void startGame() {
+    print("started=--------------------------------------");
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 100), (timer) {
+      time += 0.1;
+      height = -4.5 * time * time + 3 * time;
       setState(() {
-       
-      birdYaxis=initialHeight-height;
-     
+        birdYaxis = initialHeight - height;
+        print("yaxis value=");
+        print(birdYaxis);
+      });
+      if (birdYaxis >1) {
+        timer.cancel();
+        print("timer cancel");
+        gameHasStarted = false;
+      }
     });
     
-     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(children: [
         Expanded(
-          flex: 2,
+            flex: 2,
             child: GestureDetector(
-              onTap: Jump,
+              onTap: () {
+                if (gameHasStarted==true) {
+                  Jump();
+                } else {
+                  startGame();
+                }
+              },
               child: AnimatedContainer(
-                alignment: Alignment(0,birdYaxis),
+                alignment: Alignment(0, birdYaxis),
                 duration: Duration(milliseconds: 0),
-                      color: Colors.blueAccent,
-                      child: MyBird(),
-                    ),
+                color: Colors.blueAccent,
+                child: MyBird(),
+              ),
             )),
         Expanded(
             child: Container(
